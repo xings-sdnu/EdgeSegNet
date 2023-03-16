707b6738e814ea77dab8e29543b65d4cfc80de37
xings-sdnu <40456073+xings-sdnu@users.noreply.github.com>
2023-03-16 16:39:33 +0800

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,12 +1,1 @@
-from .fcn import FCN8
-from .unet import UNet, UNetResnet
-from .segnet import SegNet
-from .segnet import SegResNet
-from .enet import ENet
-from .gcn import GCN
-from .deeplabv3_plus import DeepLab
 from .EdgeSeg import EdgeSeg
-from .duc_hdc import DeepLab_DUC_HDC
-from .upernet import UperNet
-from .pspnet import PSPNet
-from .pspnet import PSPDenseNet
